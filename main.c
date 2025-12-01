f2f6af9ffc9bd5e71613d262283f79f08fddb30c
choipopik <144150854+choipopik@users.noreply.github.com>
2025-12-01 18:13:54 +0200

    lab 5 is done

--- main.c
+++ main.c
@@ -1,23 +1,57 @@
 #include <stdint.h>
+#include "ssd1306.h"
+
+#define TIM4_BASE       0x40000800U
+#define TIM4_CR1        (*(volatile uint32_t*)(TIM4_BASE + 0x00))
+#define TIM4_PSC        (*(volatile uint32_t*)(TIM4_BASE + 0x28))
+#define TIM4_ARR        (*(volatile uint32_t*)(TIM4_BASE + 0x2C))
+#define TIM4_EGR        (*(volatile uint32_t*)(TIM4_BASE + 0x14))
+#define TIM4_SR         (*(volatile uint32_t*)(TIM4_BASE + 0x10))
+
+#define GPIOC_BASE      0x40011000U
+#define GPIOC_CRH       (*(volatile uint32_t*)(GPIOC_BASE + 0x04))
+#define GPIOC_ODR       (*(volatile uint32_t*)(GPIOC_BASE + 0x0C))
+
+#define RCC_BASE        0x40021000U
+#define RCC_APB2ENR     (*(volatile uint32_t*)(RCC_BASE + 0x18))
+#define RCC_APB1ENR     (*(volatile uint32_t*)(RCC_BASE + 0x1C))
+
+void SPI1_Init(void);
+void SSD1306_Init(void);
+void SSD1306_DrawChessBoard(void);
+void SSD1306_Update(void);
 
 void delay(uint32_t ticks) {
     for (volatile uint32_t i = 0; i < ticks; i++);
 }
 
-void SPI1_Init(void);
-void SPI1_Write(uint8_t data);
-uint8_t SPI1_Read(void);
-void SSD1306_Init(void);
-void SSD1306_DrawChessBoard(void);
-void SSD1306_Update(void);
+void Timer4_Init(void) {
+    RCC_APB1ENR |= (1 << 2);
+    
+    TIM4_PSC = 36000 - 1;
+    TIM4_ARR = 500 - 1;
+    
+    TIM4_EGR = (1 << 0);
+    
+    TIM4_SR &= ~(1 << 0);
+    
+
+    TIM4_CR1 |= (1 << 0);
+}
 
 int main(void) {
-    *(volatile uint32_t*)(0x40021000 + 0x18) |= (1 << 4);
-    *(volatile uint32_t*)(0x40011000 + 0x04) |= (1 << 20);
+
+    RCC_APB2ENR |= (1 << 4); 
+    
+
+    GPIOC_CRH &= ~(0xF << 20); 
+    GPIOC_CRH |= (0x1 << 20); 
+    
+
     for(int i = 0; i < 3; i++) {
-        *(volatile uint32_t*)(0x40011000 + 0x0C) |= (1 << 13);
+        GPIOC_ODR &= ~(1 << 13); 
         delay(100000);
-        *(volatile uint32_t*)(0x40011000 + 0x0C) &= ~(1 << 13);
+        GPIOC_ODR |= (1 << 13);  
         delay(100000);
     }
     
@@ -26,8 +60,13 @@ int main(void) {
     SSD1306_DrawChessBoard();
     SSD1306_Update();
     
+    Timer4_Init();
+    
     while(1) {
-        *(volatile uint32_t*)(0x40011000 + 0x0C) ^= (1 << 13);
-        delay(500000);
+        if (TIM4_SR & (1 << 0)) {
+            TIM4_SR &= ~(1 << 0);
+            
+            GPIOC_ODR ^= (1 << 13);
+        }
     }
 }
